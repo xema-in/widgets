3281ecc0ad19e0f2a4c90ad70d39b46e2e7b5100
Sreenivasa Reddy Inukollu <vasu@inukollu.com>
2021-01-01 21:52:22 +0530

    test skeleton

--- projects/test/src/app/app.module.ts
+++ projects/test/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { QueueMonitorPanelModule } from 'queue-monitor-panel';
+import { TeamMonitorPanelModule } from 'team-monitor-panel';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    QueueMonitorPanelModule,
+    TeamMonitorPanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
