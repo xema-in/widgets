5e12977c38b4a9445b245ece745fe0687cece393
venkateswara.naik <venkateswara.naik@DT-IT11>
2021-01-30 12:12:25 +0530

    Queue monitor changes

--- projects/queue-monitor-panel/src/lib/queue-monitor-panel.module.ts
+++ projects/queue-monitor-panel/src/lib/queue-monitor-panel.module.ts
@@ -1,14 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { QueueMonitorPanelComponent } from './queue-monitor-panel.component';
-
-
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { TimeagoModule, TimeagoFormatter, TimeagoClock } from 'ngx-timeago';
+import { CustomClock } from '../code/custom-clock';
+import { CustomFormatter } from '../code/custom-formatter';
 
 @NgModule({
   declarations: [QueueMonitorPanelComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    TimeagoModule.forRoot({
+      formatter: { provide: TimeagoFormatter, useClass: CustomFormatter },
+      clock: { provide: TimeagoClock, useClass: CustomClock },
+    }),
+    MatCardModule,
+    MatChipsModule,
+    MatIconModule,
   ],
-  exports: [QueueMonitorPanelComponent]
+  exports: [QueueMonitorPanelComponent],
 })
-export class QueueMonitorPanelModule { }
+export class QueueMonitorPanelModule {}
