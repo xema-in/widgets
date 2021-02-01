cc941a50b8cc4d78f6e4be001bbd9239741dcfae
Sreenivasa Reddy Inukollu <vasu@inukollu.com>
2021-02-01 23:46:37 +0530

    inject timeago clock and formatter in app

--- projects/team-monitor-table-panel/src/code/custom-clock.ts
+++
@@ -1,9 +0,0 @@
-import { TimeagoClock } from 'ngx-timeago';
-import { Observable, interval } from 'rxjs';
-
-// ticks every s
-export class CustomClock extends TimeagoClock {
-  tick(then: number): Observable<number> {
-    return interval(1000);
-  }
-}
--- projects/test/src/app/app.module.ts
+++ projects/test/src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+
+import { TimeagoClock, TimeagoFormatter, TimeagoModule } from 'ngx-timeago';
+
 import { QueueMonitorPanelModule } from 'queue-monitor-panel';
 import { TeamMonitorPanelModule } from 'team-monitor-panel';
-
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TeamMonitorTablePanelModule } from 'team-monitor-table-panel';
+import { CustomFormatter } from '../code/custom-formatter';
+import { CustomClock } from '../code/custom-clock';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    TimeagoModule.forRoot({
+      formatter: { provide: TimeagoFormatter, useClass: CustomFormatter },
+      clock: { provide: TimeagoClock, useClass: CustomClock },
+    }),
     QueueMonitorPanelModule,
     TeamMonitorPanelModule,
-    BrowserAnimationsModule,
     TeamMonitorTablePanelModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
