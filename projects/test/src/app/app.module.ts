e86fe70238c76e51d17baabccaf85f314c15d00f
Sreenivasa Reddy Inukollu <vasu@inukollu.com>
2021-02-02 13:37:33 +0530

    custom clock in app module

--- projects/test/src/app/app.module.ts
+++ projects/test/src/app/app.module.ts
@@ -5,12 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 import { TimeagoClock, TimeagoFormatter, TimeagoModule } from 'ngx-timeago';
+import { CustomFormatter } from '../code/custom-formatter';
+import { CustomClock } from '../code/custom-clock';
 
 import { QueueMonitorPanelModule } from 'queue-monitor-panel';
 import { TeamMonitorPanelModule } from 'team-monitor-panel';
 import { TeamMonitorTablePanelModule } from 'team-monitor-table-panel';
-import { CustomFormatter } from '../code/custom-formatter';
-import { CustomClock } from '../code/custom-clock';
 
 @NgModule({
   declarations: [AppComponent],
